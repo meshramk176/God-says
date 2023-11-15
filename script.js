5df267d2e04da48fbc53d54c0cfd3f80b44f4b52
Kunal meshram <124501976+meshramk176@users.noreply.github.com>
2023-11-15 17:16:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 // Wait for the DOM to fully load before attaching event handlers
 document.addEventListener('DOMContentLoaded', function () {
-    var bibleVerses = [
-        "For I know the plans I have for you,” declares the Lord, “plans to prosper you and not to harm you, plans to give you hope and a future. -Jeremiah 29:11",
-        // Add more verses here
-    ];
+    // Fetch the verses from the JSON file
+    fetch('bible_verses.json')
+        .then(response => response.json())
+        .then(bibleVerses => {
+            // Now bibleVerses is an array containing your verses
 
-    // Attach an event listener to the button
-    document.getElementById('submitBtn').addEventListener('click', displayVerseWithName);
+            // Attach an event listener to the button
+            document.getElementById('submitBtn').addEventListener('click', function() {
+                displayVerseWithName(bibleVerses);
+            });
 
-    // Event listener for the Enter key in the input field
-    document.getElementById('nameInput').addEventListener('keypress', function (event) {
-        if (event.key === 'Enter') {
-            displayVerseWithName();
-        }
-    });
+            // Event listener for the Enter key in the input field
+            document.getElementById('nameInput').addEventListener('keypress', function (event) {
+                if (event.key === 'Enter') {
+                    displayVerseWithName(bibleVerses);
+                }
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching the bible verses:', error);
+        });
 
-    function displayVerseWithName() {
+    function displayVerseWithName(bibleVerses) {
         var name = document.getElementById('nameInput').value.trim();
         if (name !== "") {
             var randomIndex = Math.floor(Math.random() * bibleVerses.length);
